68487095fd9366c6d4d154fa37d04fdd2aa2e164
osamasayed <osamasayed@users.noreply.github.com>
2022-03-03 18:44:29 +0700

    Prod deploy (#1342)
    * Enable localhost Firestore logging (#1334)
    
    * fix: remove startVerse query param when switching mode (#1331)
    
    * fix: remove startVerse query param when switching mode
    
    * remove lodash omit
    
    * Remove CDN usage for fonts (#1332)
    
    * Add Tarteel attribution logging (#1335)
    
    * Add Tarteel attribution logging
    
    * Updates
    
    * translation view cell actions, no hover effect on mobile (#1322)
    
    * translation view cell actions, no hover effect on mobile
    
    * .
    
    * remove hover effect on mobile
    
    * dismiss keyboard on search result clicked (#1326)
    
    * dismiss keyboard on search result clicked
    
    * fix build error
    
    * Fix chapterId with leading 00 (#1337)
    
    * Update fonts path (#1336)
    
    * Fix 16-line Indopak wrong page nav button (#1338)
    
    * fix: settings drawer hidden when audio auto scroll (#1340)
    
    * fix: settings drawer hidden when audio auto scroll
    
    * update naming
    
    Co-authored-by: Muhammad Muhajir <muhajirframe@gmail.com>

--- src/components/Fonts/FontPreLoader.tsx
+++ src/components/Fonts/FontPreLoader.tsx
@@ -2,15 +2,13 @@ import React from 'react';
 
 import Head from 'next/head';
 
-import { makeCDNUrl } from 'src/utils/cdn';
-
 const DEFAULT_LOCALE = 'en';
 
 const LOCALE_PRELOADED_FONTS = {
-  [DEFAULT_LOCALE]: [{ type: 'font/woff2', location: 'fonts/lang/ProximaVara/ProximaVara.woff2' }],
-  ar: [{ type: 'font/woff2', location: 'fonts/lang/arabic/NotoNaskhArabic-Regular.woff2' }],
-  bn: [{ type: 'font/ttf', location: 'fonts/lang/bengali/NotoSerifBengali-Regular.woff2' }],
-  ur: [{ type: 'font/woff2', location: 'fonts/lang/urdu/MehrNastaliqWeb.woff2' }],
+  [DEFAULT_LOCALE]: [{ type: 'font/woff2', location: '/fonts/lang/ProximaVara/ProximaVara.woff2' }],
+  ar: [{ type: 'font/woff2', location: '/fonts/lang/arabic/NotoNaskhArabic-Regular.woff2' }],
+  bn: [{ type: 'font/ttf', location: '/fonts/lang/bengali/NotoSerifBengali-Regular.woff2' }],
+  ur: [{ type: 'font/woff2', location: '/fonts/lang/urdu/MehrNastaliqWeb.woff2' }],
 } as Record<string, { type: string; location: string }[]>;
 
 interface Props {
@@ -28,7 +26,7 @@ const FontPreLoader: React.FC<Props> = ({ locale }) => {
           rel="preload"
           as="font"
           type={fontDetails.type}
-          href={makeCDNUrl(fontDetails.location)}
+          href={fontDetails.location}
           crossOrigin="anonymous"
         />
       ))}
